85091a0812fff7545784014a02a63fcea3de27ec
victormmorales <victor.morales@criptan.com>
2022-04-13 14:42:59 +0200

    prespuesto

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,17 @@
+const ControlPresupuesto = ({ presupuesto }) => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <p>Grafica aqui</p>
+      </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto: </span>
+          {presupuesto} €
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
